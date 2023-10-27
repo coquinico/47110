9364ce530bb20dba7dec4a8dda047b5640245c83
Lucrecia Nicolao <coquinico@gmail.com>
2023-10-27 10:59:18 -0300

    Segunda Pre-Entrega Lucrecia Nicolao

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,26 @@
-
 const productos = [
-    { nombre: "Medias", precio: 10 },
-    { nombre: "Bolso", precio: 50 },
-    { nombre: "Enterito", precio: 100 },
-    { nombre: "Guantes", precio: 35 },
-    { nombre: "Guantes Everlast", precio: 80 }
+    { nombre: "Medias", precio: 10, stock: 50 },
+    { nombre: "Bolso", precio: 50, stock: 20 },
+    { nombre: "Enterito", precio: 100, stock: 10 },
+    { nombre: "Guantes", precio: 35, stock: 30 },
+    { nombre: "Guantes Everlast", precio: 80, stock: 15 }
 ];
+
+function restarDelStock(producto, cantidad) {
+    if (producto.stock >= cantidad) {
+        producto.stock -= cantidad;
+        return true; 
+    } else {
+        return false; 
+    }
+}
 
 // funcion costo del producto
 function calcularCostoProducto(producto, cantidad) {
     return producto.precio * cantidad;
 }
 
-// Buscar un producto por nombre o número
+// buscar un producto por nombre o número
 function buscarProducto(entrada) {
     if (!isNaN(entrada)) {
         const index = parseInt(entrada) - 1;
@@ -25,7 +33,7 @@ function buscarProducto(entrada) {
     return null;
 }
 
-// Función costo total del carrito
+// función costo total del carrito
 function calcularCostoTotalCarrito(funcionCostoProducto, carrito) {
     let costoTotal = 0;
     carrito.forEach(item => {
@@ -71,14 +79,21 @@ while (true) {
     let costoProducto = calcularCostoProducto(producto, cantidad);
 
     if (costoProducto > 0) {
-        costoTotal += costoProducto;
-        alert(`Costo del producto ${producto.nombre}: $${costoProducto}`);
+        let seResto = restarDelStock(producto, cantidad);
+        if (seResto) {
+            costoTotal += costoProducto;
+            alert(`Costo del producto ${producto.nombre}: $${costoProducto}`);
+        } else {
+            alert("No hay suficiente stock de este producto.");
+        }
     }
 }
+
+
 
 if (costoTotal > 0) {
     alert(`Costo total de los productos elegidos: $${costoTotal}`);
 } else {
     alert("No se ha seleccionado ningún producto.");
 }
-let costoTotalCarrito = calcularCostoTotalCarrito(calcularCostoProducto, carrito);
+
